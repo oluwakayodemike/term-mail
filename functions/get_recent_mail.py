4aa47b27563719afeed932aef2f268320f486ce0
maik <michaelaykay@gmail.com>
2026-08-10 05:45:11 -0700

    feat: can now read the recent -N of mail you want from the mail

---
+++ functions/get_recent_mail.py
@@ -0,0 +1,43 @@
+from googleapiclient.errors import HttpError
+import html
+import sys
+from pathlib import Path
+
+parent_dir = Path(__file__).resolve().parent.parent
+
+if str(parent_dir) not in sys.path:
+    sys.path.append(str(parent_dir))
+    
+from auth import get_gmail_service
+
+def get_recent_mail(limit: int = 5):
+    try:
+        service = get_gmail_service()
+        results = service.users().messages().list(userId="me", labelIds=['INBOX'], maxResults=limit).execute()
+        messages = results.get('messages', [])
+
+        if not messages:
+            print("inbox is empty!")
+        for msg in messages:
+            # using format as metadata to grab only headers makes it very fast
+            msg_data = service.users().messages().get(
+                userId="me",
+                id=msg["id"],
+                format="metadata",
+                metadataHeaders=["Subject"]
+            ).execute()
+
+            headers = msg_data["payload"]["headers"]
+            subject = "Absolutely Nothing"
+
+            for header in headers:
+                if header["name"].lower() == 'subject':
+                    subject = header["value"]
+                    break
+
+            snippet = msg_data.get('snippet', '')
+
+            print(f"- {subject}")
+            print(f"    {snippet}...\n")
+    except HttpError as error:
+        raise Exception(f"An error occured: {error}")
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import argparse
 from auth import get_gmail_service
+from functions.get_recent_mail import get_recent_mail
 
 def main():
     parser = argparse.ArgumentParser(description="Term-Mail: Your Terminal Email Client")
     parser.add_argument("--login", action="store_true", help="Connect to Google and test authentication")
+    parser.add_argument("--recent", type=int, metavar="N", help="Read the N of most recent emails")
     args = parser.parse_args()
 
     if args.login:
@@ -12,6 +14,10 @@ def main():
         service = get_gmail_service()
         if service:
             print("Success...You are fully authenticated!.")
+            
+    elif args.recent:
+        get_recent_mail(limit=args.recent)
+        
     else:
         parser.print_help()
         
